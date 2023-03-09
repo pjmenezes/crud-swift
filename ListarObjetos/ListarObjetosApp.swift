010c3ccea1e0eba92a7443c64cc04dbdee178ab9
Pedro Menezes <pmenezes1hp@gmail.com>
2023-03-09 18:22:43 -0400

    Initial Commit

---
+++ ListarObjetos/ListarObjetosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListarObjetosApp.swift
+//  ListarObjetos
+//
+//  Created by pedro on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ListarObjetosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
